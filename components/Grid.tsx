9458dd38010e3ef6e3eede6c8c1a8c0985a167c0
Harsh-Pathak3601 <pathakharsh3601@gmail.com>
2025-12-22 22:23:16 +0530

    Improved version

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -3,16 +3,35 @@ import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
-    <section id="about">
-      <BentoGrid className="w-full py-20">
+    <section
+      id="about"
+      className="relative
+    z-10
+    pt-32
+    pb-20
+    min-h-fit
+    scroll-mt-32
+  "   // section spacing
+    >
+      {/* 🔹 SECTION TITLE */}
+      <div className="mb-12 text-center px-4">
+        <h1 className="heading text-white">
+          Inside My <span className="text-purple">World</span>
+        </h1>
+
+        <p className="mt-3 text-sm sm:text-base md:text-lg lg:text-2xl md:tracking-wider text-white font-light max-w-2xl mx-auto">
+          A snapshot of my work, skills, and developer mindset.
+        </p>
+      </div>
+
+      {/* 🔹 BENTO GRID */}
+      <BentoGrid className="w-full">
         {gridItems.map((item, i) => (
           <BentoGridItem
             id={item.id}
             key={i}
             title={item.title}
             description={item.description}
-            // remove icon prop
-            // remove original classname condition
             className={item.className}
             img={item.img}
             imgClassName={item.imgClassName}
