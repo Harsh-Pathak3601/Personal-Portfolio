8296805a3c43a2a32f5f4aae3d0c23b5753416e6
Harsh-Pathak3601 <pathakharsh3601@gmail.com>
2025-12-22 18:59:57 +0530

    Improved version

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -9,6 +9,7 @@ import {
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
+
 export const FloatingNav = ({
   navItems,
   className,
@@ -16,7 +17,7 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
-    icon: React.ReactNode;
+    icon?: React.ReactNode;
   }[];
   className?: string;
 }) => {
